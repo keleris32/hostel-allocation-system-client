4d453eb03d5f15173040e2d887dcfad5083c8df5
keleris32 <charles.ejiegbu@gmail.com>
2022-02-11 00:14:50 +0100

    feat: setup home page to get & set access_token

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import React, { useEffect, useState } from 'react';
+import AppRoutes from './routes/Routes';
+import { setAccessToken } from './utils/accessToken';
+import axiosInstance from './utils/axiosInterceptor';
+
 function App() {
-  return <div className="App"></div>;
+  const [loading, setLoading] = useState(true);
+
+  const fetchRefreshToken = async (): Promise<void> => {
+    try {
+      const response = await axiosInstance.post('refresh-token');
+
+      setAccessToken(response.data.acess_token);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchRefreshToken();
+  }, []);
+
+  if (loading) {
+    return (
+      <div>
+        <h1>Loadinggggggg whole app </h1>
+      </div>
+    );
+  }
+
+  return <AppRoutes />;
 }
 
 export default App;

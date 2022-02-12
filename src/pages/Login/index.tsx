4c3f1cf046445a2d3274eff53eb586ff30f3a2af
keleris32 <charles.ejiegbu@gmail.com>
2022-02-12 05:35:19 +0100

    feat: setup login page, its validation schema and styling

--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,9 +1,89 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { getAccessToken } from '../../utils/accessToken';
+import { Formik } from 'formik';
+import { CircularProgress } from '@mui/material';
+import { loginValidationSchema } from './validationSchema';
+import { loginStudent } from '../../context/actions/loginStudent';
+import { useContext } from 'react';
+import { GlobalContext } from '../../context/provider';
+import salemLogo from '../../assets/salemunilogo.png';
 
-function Login(): React.ReactNode {
+interface FormPropTypes {
+  matricNumber: string;
+  password: string;
+}
+
+function Login(): JSX.Element {
+  const navigate = useNavigate();
+  const token = getAccessToken();
+
+  if (token) {
+    navigate('/');
+  }
+
+  const {
+    // @ts-ignore
+    authDispatch,
+    // @ts-ignore
+    authState: { loginLoading },
+  } = useContext(GlobalContext);
+
+  const submitForm = (formData: FormPropTypes) => {
+    loginStudent(formData)(authDispatch);
+  };
+
   return (
-    <div>
-      <h1>Helllo Login!</h1>
+    <div className="auth-body">
+      <div className="auth-form-con">
+        <div className="auth-app-logo">
+          <img src={salemLogo} alt="School logo" />
+        </div>
+        <Formik
+          initialValues={{ matricNumber: '', password: '' }}
+          validateOnMount={true}
+          validationSchema={loginValidationSchema}
+          onSubmit={(values) => submitForm(values)}
+        >
+          {(props) => (
+            <form action="">
+              <h2>Login</h2>
+              <input
+                type="text"
+                name="matricNumber"
+                placeholder="Matric Number"
+                onChange={props.handleChange('matricNumber')}
+                onBlur={props.handleBlur('matricNumber')}
+                value={props.values.matricNumber.trim()}
+              />
+              {props.errors.matricNumber && props.touched.matricNumber && (
+                <p className="auth-errors">{props.errors.matricNumber}</p>
+              )}
+              <input
+                type="password"
+                name="password"
+                placeholder="Password"
+                onChange={props.handleChange('password')}
+                onBlur={props.handleBlur('password')}
+                value={props.values.password.trim()}
+              />
+              {props.errors.password && props.touched.password && (
+                <p className="auth-errors">{props.errors.password}</p>
+              )}
+              <button type="submit" disabled={loginLoading}>
+                {loginLoading ? (
+                  <CircularProgress style={{ color: '#fff' }} size={25} />
+                ) : (
+                  'Login to portal'
+                )}
+              </button>
+            </form>
+          )}
+        </Formik>
+        <span className="auth-form-link">
+          Don't have an account?{' '}
+          <span className="auth-form-active-link">Register here!</span>
+        </span>
+      </div>
     </div>
   );
 }
---
+++ src/pages/Login/validationSchema.ts
@@ -0,0 +1,21 @@
+import * as yup from 'yup';
+
+export const loginValidationSchema = yup.object().shape({
+  matricNumber: yup
+    .string()
+    .trim()
+    .min(
+      10,
+      ({ min }) => `Matric Number must be at least ${min} characters long`
+    )
+    .max(
+      15,
+      ({ max }) => `Matric Number must be at most ${max} characters long`
+    )
+    .required('Matric Number is required'),
+  password: yup
+    .string()
+    .trim()
+    .min(8, ({ min }) => `Password must be at least ${min} characters long`)
+    .required('Password is required'),
+});

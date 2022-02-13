bf36ca6067cd1b4366809637ea901b487710e712
keleris32 <charles.ejiegbu@gmail.com>
2022-02-13 16:56:19 +0100

    feat: store token & user_id in storage

--- src/context/actions/registerStudent.ts
+++ src/context/actions/registerStudent.ts
@@ -1,33 +1,48 @@
 import { authConstants } from '../../enums/authEnums';
 import axiosInstance from '../../utils/axiosInterceptor';
 import { setAccessToken } from '../../utils/accessToken';
+import { RegistrationFormTypes } from '../../pages/Register';
+import { NavigateFunction } from 'react-router-dom';
+import { setStudent } from '../../utils/storageUtils';
 
 export const registerStudent =
   // @ts-ignore
 
 
-    ({ matricNumber: matric_no, password }) =>
+    (
+      registrationFormData: RegistrationFormTypes,
+      setIsErrorAlertActive: React.Dispatch<React.SetStateAction<boolean>>,
+      navigate: NavigateFunction
+    ) =>
     // @ts-ignore
     async (dispatch) => {
-      dispatch({
+      await dispatch({
         type: authConstants.REGISTER_LOADING,
       });
       try {
-        const response = await axiosInstance.post('auth/login', {
-          matric_no,
-          password,
-        });
+        const response = await axiosInstance.post(
+          'auth/register',
+          registrationFormData
+        );
 
         setAccessToken(response.data.data.access_token);
 
-        dispatch({
+        setStudent(response.data.data.id);
+
+        await dispatch({
           type: authConstants.REGISTER_SUCCESS,
           payload: response.data.data,
         });
+
+        navigate('/');
       } catch (error: any) {
-        dispatch({
+        await dispatch({
           type: authConstants.REGISTER_ERROR,
-          payload: error?.message,
+          payload:
+            error.response.data.message ??
+            'Please check your internet connection and try again later!',
         });
+
+        setIsErrorAlertActive(true);
       }
     };

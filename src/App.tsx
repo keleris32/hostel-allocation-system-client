b5ce2403911441eaadca3bea317346c832a04a97
keleris32 <charles.ejiegbu@gmail.com>
2022-02-15 17:22:50 +0100

    fix: discarded refresh token

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,37 @@
-import { useEffect, useState } from 'react';
-import { Preloader } from './components';
+// import { useEffect, useState } from 'react';
+// import { Preloader } from './components';
 import AppRoutes from './routes/Routes';
-import { setAccessToken } from './utils/accessToken';
-import axiosInstance from './utils/axiosInterceptor';
+// import { setAccessToken } from './utils/accessToken';
+// import axiosInstance from './utils/axiosInterceptor';
 
 function App() {
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
 
-  const fetchRefreshToken = async (): Promise<void> => {
-    try {
-      const response = await axiosInstance.post('refresh-token');
+  // const fetchRefreshToken = async (): Promise<void> => {
+  //   try {
+  //     const response = await axiosInstance.post('refresh-token');
 
-      if (response.data.access_token) {
-        setAccessToken(response.data.acess_token);
-      }
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  //     if (response.data.access_token) {
+  //       setAccessToken(response.data.acess_token);
+  //     }
+  //   } catch (error) {
+  //     console.log(error);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // };
 
-  useEffect(() => {
-    fetchRefreshToken();
-  }, []);
+  // useEffect(() => {
+  //   fetchRefreshToken();
+  // }, []);
 
-  if (loading) {
-    return (
-      <div>
-        <Preloader />
-      </div>
-    );
-  }
+  // if (loading) {
+  //   return (
+  //     <div>
+  //       <Preloader />
+  //     </div>
+  //   );
+  // }
 
   return <AppRoutes />;
 }

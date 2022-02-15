8641d4ef7af44e9ed6977a66a109c50e0de0ff4f
keleris32 <charles.ejiegbu@gmail.com>
2022-02-15 12:50:46 +0100

    feat: added Preloader comp to page on load

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from 'react';
-// import { useNavigate } from 'react-router-dom';
+import { Preloader } from './components';
 import AppRoutes from './routes/Routes';
 import { setAccessToken } from './utils/accessToken';
 import axiosInstance from './utils/axiosInterceptor';
 
 function App() {
-  // const navigate = useNavigate();
   const [loading, setLoading] = useState(true);
 
   const fetchRefreshToken = async (): Promise<void> => {
@@ -17,7 +16,6 @@ function App() {
       }
     } catch (error) {
       console.log(error);
-      // navigate('login');
     } finally {
       setLoading(false);
     }
@@ -30,7 +28,7 @@ function App() {
   if (loading) {
     return (
       <div>
-        <h1>Loadinggggggg whole app </h1>
+        <Preloader />
       </div>
     );
   }
